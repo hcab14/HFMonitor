64b02a6f25f28d2f18e9063255ea7c80c9087868
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2010-10-05 12:32:10 +0000

    improved IQBuffer.hpp

--- trunk/HFMonitor/IQBuffer.hpp
+++ trunk/HFMonitor/IQBuffer.hpp
@@ -7,6 +7,34 @@
 #include <iterator>
 #include <vector>
 #include <complex>
+#include <stdexcept>
+#include <boost/bind.hpp>
+
+namespace Internal {
+  // modulo counter without using the % operation
+  template<typename T>
+  class ModuloCounter {
+  public:
+    explicit ModuloCounter(T m) : m_(m), t_(0) {}    
+
+    T m() const { return m_; }
+    operator T() const { return t_; }
+
+    ModuloCounter& operator++() {
+      const T tInc(t_+1);
+      t_ = (tInc >= m_) ? T(0) : tInc;
+      return *this;
+    }
+    ModuloCounter& operator--() {
+      t_ = (t_ == T(0)) ? m_-1 : t_-1;
+      return *this;
+    }
+
+  private:
+    const T m_;
+    T t_;
+  } ;
+}
 
 class IQBuffer {
 public:
@@ -16,56 +44,74 @@ public:
 	   double overlap) // overlap [0..1]
     : iqVec_(2*n,0)
     , n_(n)
-    , m_((1.0-overlap)*n_+0.5)
-    , lastProcessedIndex_(0)
-    , counter_(0) {}
+    , m_(size_t((1.0-overlap)*n_+0.5))
+    , lpi_(0)
+    , counterModN_(n_)
+    , counterModM_(m_)
+    , counterModNM_(m_*n_) {
+    if (overlap < 0.0 || overlap > 1.0)
+      throw std::runtime_error("IQBuffer: overlap < 0.0 || overlap > 1.0");
+  }
 
-  // Samples samples() const {
-  //   Samples s;
-  //   const size_t i(counter_ % n_);
-  //   for (size_t u(i); u<n_; ++u)
-  //     s.push_back(iqVec_[u]);
-  //   return s;
-  // }
-  size_t counter() const { return counter_; }
+  virtual ~IQBuffer() {}
+
+  // returns the remaining unprocessed samples
+  Samples samples() const {
+    Samples s;
+    std::copy(iqVec_.begin()+lastI0(), iqVec_.begin()+lastI1(), 
+	      std::back_inserter(s));
+    return s;
+  }
+
   size_t n() const { return n_; }
   size_t m() const { return m_; }
   double overlap() const { return 1.0-double(m_)/double(n_); }
-  
+ 
+  // insert a range of samples
+  void insert(Samples::const_iterator begin, Samples::const_iterator end) {
+    void (IQBuffer::*fp)(Complex)(&IQBuffer::insert);
+    std::for_each(begin, end, boost::bind(fp, this, _1));
+  }
+  // insert a single sample
   void insert(Complex c) {
-    const size_t i(counter_ % n_);
-    const size_t lastI0((m_*(1+(counter_)/m_) % n_));
-    const size_t lastI1(i+n_);
-    std::cout << "counter_,i= " << counter_ << " " << i << " " << lastI0 << " : " ;
-    std::copy(iqVec_.begin(), iqVec_.end(), std::ostream_iterator<std::complex<double> >(std::cout, " "));
-    std::cout << std::endl;
-    lastIQ(iqVec_.begin()+lastI0, iqVec_.begin()+lastI1);
-    if (counter_ % m_ == 0) {
-      // std::cout << "*** procIQ: " << i << "-" << i+n_ << std::endl;;
-      procIQ(iqVec_.begin()+i, iqVec_.begin()+i+n_);
-      lastProcessedIndex_ = i+n_;
+    if (counterModM_ == 0) {
+      procIQ(iqVec_.begin()+counterModN_, iqVec_.begin()+counterModN_+n_);
+      lpi_ = (counterModN_+m_) % n_;
     }
-    iqVec_[i] = iqVec_[i+n_] = c;
-    counter_ = (1+counter_ == m_*n_) ? 0 : 1+counter_;
+    iqVec_[counterModN_] = iqVec_[counterModN_+n_] = c;
+    ++counterModN_;
+    ++counterModM_;
+    ++counterModNM_;
   }
 
-  virtual void procIQ(Samples::const_iterator i0, Samples::const_iterator i1) {
+  virtual void procIQ(Samples::const_iterator i0, 
+		      Samples::const_iterator i1) {
     std::cout << "*** procIQ ";
-    std::copy(i0,i1, std::ostream_iterator<std::complex<double> >(std::cout, " "));
-    std::cout << std::endl;
+    std::copy(i0, i1, 
+	      std::ostream_iterator<std::complex<double> >(std::cout, " "));
+    std::cout << "***" << std::endl;
   }
-  virtual void lastIQ(Samples::const_iterator i0, Samples::const_iterator i1) {
-    std::cout << "    lastIQ ";
-    std::copy(i0,i1, std::ostream_iterator<std::complex<double> >(std::cout, " "));
-    std::cout << std::endl;
-  }
+
 protected:
 private:
+  // returns first index of not yet processed samples
+  size_t lastI0() const {
+    return lpi_;
+  }
+  // returns last index of not yet processed samples
+  size_t lastI1() const {
+    return (counterModN_+n_ - lastI0() > n_ 
+	    ? size_t(counterModN_) 
+	    : counterModN_+n_);
+  }
+
   Samples iqVec_;
-  size_t n_;
-  size_t m_;
-  size_t lastProcessedIndex_;
-  size_t counter_;
+  const size_t n_;
+  const size_t m_;
+  size_t lpi_;
+  Internal::ModuloCounter<size_t> counterModN_;
+  Internal::ModuloCounter<size_t> counterModM_;
+  Internal::ModuloCounter<size_t> counterModNM_;
 } ;
 
 
--- trunk/HFMonitor/testIQBuffer_main.cpp
+++ trunk/HFMonitor/testIQBuffer_main.cpp
@@ -3,7 +3,24 @@
 #include "IQBuffer.hpp"
 
 int main() {
-  IQBuffer b(4, 0.75);
-  for (size_t u(0); u<100; ++u) 
+  Internal::ModuloCounter<size_t> m(4);
+
+  for (size_t u(0); u<10; ++u) {
+    ++m;
+    std::cout << u << " " << m << std::endl;
+  }
+  for (size_t u(0); u<10; ++u) {
+    --m;
+    std::cout << u << " " << m << std::endl;
+  }
+
+  IQBuffer b(7, 0.5);
+  for (size_t u(0); u<100; ++u) {
+    std::cout << "------------------------------\n";
     b.insert(std::complex<double>(u,0));
+    const IQBuffer::Samples s(b.samples());
+    std::cout << "--- lastIQ ";
+    std::copy(s.begin(),s.end(), std::ostream_iterator<std::complex<double> >(std::cout, " "));
+    std::cout << std::endl;
+  }
 }
