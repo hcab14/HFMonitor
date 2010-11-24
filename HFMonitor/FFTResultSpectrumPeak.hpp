f1af4bbe4342a15196912b074b6ce35c422c3ae2
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2010-11-24 10:41:37 +0000

    improved background noise level estimation in FFTResultSpectrumPeak

--- HFMonitor/FFTResultSpectrumPeak.hpp
+++ HFMonitor/FFTResultSpectrumPeak.hpp
@@ -31,40 +31,75 @@ namespace Result {
     bool findPeak(const SpectrumBase& s, const PowerSpectrum& ps, double minRatio) {
       return ps.empty() ? false : findPeak(s, ps, ps.front().first, ps.back().first, minRatio);
     }
+    // find a peak in the range [fMin, fMax] (including fMax)
     bool findPeak(const SpectrumBase& s, const PowerSpectrum& ps,
                   double fMin, double fMax, double minRatio) {
       const size_t i0(ps.freq2index(fMin));
       const size_t i1(ps.freq2index(fMax));
+      // PowerSpectrum::const_iterator
+      //   iMin(std::min_element(ps.begin()+i0, ps.begin()+i1+1,
+      //                         PowerSpectrum::cmpSecond));
       PowerSpectrum::const_iterator
-        iMin(std::min_element(ps.begin()+i0, ps.begin()+i1, PowerSpectrum::cmpSecond));
-      PowerSpectrum::const_iterator
-        iMax(std::max_element(ps.begin()+i0, ps.begin()+i1, PowerSpectrum::cmpSecond));
+        iMax(std::max_element(ps.begin()+i0, ps.begin()+i1+1,
+                              PowerSpectrum::cmpSecond));
       
       const size_t indexMax(std::distance(ps.begin(), iMax));
 
+      // estimate frequency 
+      const std::pair<double, double> 
+        peakFrequency(estimatePeakFrequency(ps, indexMax));
+
+      // estimate background
+      const std::pair<double, double> 
+        background(estimateBackground(ps, indexMax, i0, i1));
+      
+      // for (int i=-2; i<=2; ++i) {
+      //   std::cout << "FP_ " << ps[indexMax+i].first << " " << ps[indexMax+i].second << " "
+      //             << std::arg(s[s.freq2index(ps[indexMax+i].first)])/(2*M_PI) 
+      //   	  << std::endl;
+      // }
+
+      ratio_ = (background.first != 0.0) ? iMax->second / background.first : 1.0;
+      if (ratio_ < minRatio)
+        std::cout << "ratio < minRatio : " << ratio_ << " " 
+                  << background.first << " " 
+                  << background.second << std::endl;
+
+      // TODO: error propagation
+      fMeasured_    = peakFrequency.first;
+      fMeasuredRMS_ = peakFrequency.second;
+      strength_     = (ratio_ > minRatio) ? iMax->second     : background.first;
+      strengthRMS_  = (ratio_ > minRatio) ? iMax->second/10. : background.second; // TODO
+      return (ratio_ > minRatio);
+    }
+
+    std::pair<double, double> 
+    estimatePeakFrequency(const PowerSpectrum& ps, size_t indexMax) const {
       double sum(0.0);
       double weight(0.0);
-      for (size_t u(indexMax-std::min(indexMax, size_t(3))); u<(indexMax+4) && u < ps.size(); ++u) {
+      for (size_t u(indexMax-std::min(indexMax, size_t(3))); 
+           u<(indexMax+4) && u < ps.size(); ++u) {
+        sum    += ps[u].second * ps[u].first;
         weight += ps[u].second;
-        sum += ps[u].second * ps[u].first;
       }
-      for (int i=-2; i<=2; ++i) {
-        std::cout << "FP_ " << ps[indexMax+i].first << " " << ps[indexMax+i].second << " "
-                  << std::arg(s[s.freq2index(ps[indexMax+i].first)])/(2*M_PI) 
-		  << std::endl;
-      }
-      ratio_ = (iMin->second != 0.0) ? iMax->second / iMin->second : 1.0;
-      if (ratio_ < minRatio || weight == 0.0) {
-        std::cout << "ratio < minRatio || weight == 0.0: " << ratio_ << " " << sum << " " << weight << std::endl;
-        return false;
-      }
-      // TODO: error propagation
-      const std::pair<double, double> c(cal(sum/weight));
-      fMeasured_    = c.first;
-      fMeasuredRMS_ = c.second;
-      strength_     = iMax->second;
-      strengthRMS_  = iMax->second/10.; // TODO
-      return true;
+      return (weight != 0.0) ? cal(sum/weight) : std::make_pair(0.0, 1.0);
+    }
+
+    std::pair<double, double> 
+    estimateBackground(const PowerSpectrum& ps, size_t indexMax,
+                       size_t i0, size_t i1) const {
+      double sum(0.0), sum2(0.0);
+      size_t counter(0);
+      for (size_t u(i0); u<i1+1; ++u) {
+        if (std::abs(int(u) - int(indexMax)) < 4) continue;
+        sum += ps[u].second;
+        sum2 += ps[u].second * ps[u].second;
+        ++counter;
+      }      
+      return (counter != 0) 
+        ? std::make_pair(sum/counter, 
+                         std::sqrt(sum2/counter - sum*sum/(counter*counter)))
+        : std::make_pair(0., 1.);
     }
 
     virtual ~SpectrumPeak() {}
