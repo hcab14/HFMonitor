525e4808d9d302fe83e29c1704403f77688978bc
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2011-01-09 18:34:51 +0000

    update, hfmon_server w/out ptime filter

--- HFMonitor/start_client.sh
+++ HFMonitor/start_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # $Id$
 at now <<EOF
-cd /media/dfa46bc2-9377-47b4-8d25-3cc2039fc2ff/.rw/home/linutop
-~/HFMonitor/hfmon_clientTCPFFT 127.0.0.1 18000 ~/HFMonitor/config_FFTProcessor.xml > ~/HFMonitor/log_client.txt 2>&1
+cd /home/linutop/HFMonitor
+./hfmon_clientTCPFFT 127.0.0.1 18000 config_FFTProcessor.xml > log_client.txt 2>&1
 EOF
