7d182618a13b2adfa12b0683968b1036b8fa58be
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2012-01-25 11:19:20 +0000

    updated scripts

---
+++ HFMonitor/check_running.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# $Id$
+
+function check_client {
+    if [ -f .pid_client ]; then
+	echo YES
+    else
+	echo NO
+    fi
+}
+function check_server {
+    if [ `check_client` == YES ] || [ -f .pid_server ]; then
+	echo YES
+    else
+	echo NO
+    fi
+}
+
+function restart_client {    
+    ./stop.sh client > /dev/null
+    sleep 5s
+    ./start.sh client > /dev/null
+    echo FAIL_client
+}
+function restart_server {
+    ./stop.sh server > /dev/null
+    sleep 10s
+    ./start.sh server > /dev/null
+    sleep 30s
+    echo FAIL_server
+}
+function restart_client_server {
+    ./stop.sh client > /dev/null
+    sleep 5s
+    ./stop.sh server > /dev/null
+    sleep 10s
+    ./start.sh server > /dev/null
+    sleep 30s
+    ./start.sh client > /dev/null
+    echo FAIL_server_client
+}
+
+function check_running {    
+    local status_client=`./status.sh client` 
+    local status_server=`./status.sh server`
+    
+    if [ `check_client` == YES ]; then 
+	[ ${status_client} == RUN ] && [ ${status_server} == RUN ] && echo OK
+	[ ${status_client} != RUN ] && [ ${status_server} == RUN ] && restart_client
+	[ ${status_client} == RUN ] && [ ${status_server} != RUN ] && restart_client_server
+	[ ${status_client} != RUN ] && [ ${status_server} != RUN ] && restart_client_server
+    else	
+	if [ `check_server` == YES ]; then 
+	    [ ${status_server} == RUN ] && echo OK
+	    [ ${status_server} == RUN ] && restart_server
+	else 
+	    echo OFF
+	fi
+    fi
+}
+
+function check_for_locks {
+    ls .lock_* > /dev/null 2>&1
+    [ $? == 0 ] && echo LOCKED || echo FREE
+}
+
+LOG_STATUS=log_status.txt
+
+function last_status {
+    if [ -f ${LOG_STATUS} ]; then
+	tail -1 ${LOG_STATUS} | awk '{print $2}'
+    else
+	echo FAIL
+    fi
+}
+
+if [ `check_for_locks` == FREE ]; then
+    result=`check_running`
+    echo result=$result last_status=`last_status`
+    
+    if [[ `last_status` != OK ]] || [[ $result != OK ]]; then
+	echo `date +%Y-%m-%dT%H:%M:%S` $result >> ${LOG_STATUS}
+    fi
+fi
--- HFMonitor/start.sh
+++ HFMonitor/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # $Id$
-cd /home/chm/C++/HFMonitor
 
 function start_server {
 cat <<EOF
@@ -18,22 +17,35 @@ echo \$! > .pid_client
 EOF
 }
 
-what=$1
-
-if [ `./status.sh $what` == RUN ]; then
-    echo "$what is already running"
-    exit 1
-fi
-
-case $what in
-    server|client)
-	start_$what > .start_$what.sh
-	chmod u+x .start_$what.sh
-	at now <<EOF
+echo -n > .lock_start_$$
+if [ $# == 0 ]; then
+    status_client=`./status.sh client` 
+    status_server=`./status.sh server`
+    if [ ${status_client} == RUN ] && [ ${status_server} == RUN ]; then 
+	echo OK: client and server are running
+    else
+	[ ${status_server} != RUN ] && ( $0 server; sleep 30s )
+	[ ${status_client} != RUN ] && ( $0 client )
+    fi
+else
+    what=$1
+    
+    if [ `./status.sh $what` == RUN ]; then
+	echo "$what is already running"
+	exit 1
+    fi
+    
+    case $what in
+	server|client)
+	    start_$what > .start_$what.sh
+	    chmod u+x .start_$what.sh
+	    at now <<EOF
 ./.start_$what.sh
 EOF
-	;;
-    *)
-	echo "unrecognized \"$what\". Allowed services: {server,client}"
-	;;
-esac
+	    ;;
+	*)
+	    echo "unrecognized \"$what\". Allowed services: {server,client}"
+	    ;;
+    esac
+fi
+rm -f .lock_start_$$
