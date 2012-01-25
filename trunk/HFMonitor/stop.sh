8832fb6affcf6e3da0ac2d7ae7efeee683927dfe
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2012-01-25 11:19:20 +0000

    updated scripts

--- trunk/HFMonitor/stop.sh
+++ trunk/HFMonitor/stop.sh
@@ -6,23 +6,33 @@ function check_run {
     echo $(( $pid ))
 }
 
-what=$1
-pid_file=.pid_$what
+echo -n > .lock_stop_$$
+if [ $# == 0 ]; then
+    $0 client
+    $0 server
+else
 
-if [ -f $pid_file ]; then
-    pid=`cat $pid_file`
-    if [ X$pid == X`check_run $pid` ]; then
-	sudo kill $pid
-	sleep 1s
-        ## check if stop worked
+    what=$1
+    pid_file=.pid_$what
+    
+    if [ -f $pid_file ]; then
+	pid=`cat $pid_file`
 	if [ X$pid == X`check_run $pid` ]; then
-	    echo "$what ($pid) stop failed. Process is still running"
+	    sudo kill $pid
+	    sleep 1s
+        ## check if stop worked
+	    if [ X$pid == X`check_run $pid` ]; then
+		echo "$what ($pid) stop failed. Process is still running"
+	    else
+		echo "$what ($pid) stopped"
+		rm -f $pid_file
+	    fi
 	else
-	    echo "$what ($pid) stopped"
+	    echo "$what ($pid) not running"
+	    rm -f $pid_file
 	fi
     else
-	echo "$what ($pid) not running"
-    fi
-else
-    echo "$pid_file does not exist"
+	echo "$pid_file does not exist"
+    fi    
 fi
+rm -f .lock_stop_$$
