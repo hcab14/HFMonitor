c9058f3444a51e371b6c357c35b0191ba8999e26
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2012-03-29 11:32:51 +0000

    prop update

---
+++ tags/FiFiDAQ_v1.1/ps_vlf.m
@@ -0,0 +1,31 @@
+# -*- octave -*-
+# $Id$
+#
+# filenames: cell array of input filenames
+#
+#  examples 1) plot_vlf({"file1", "file2"}), or
+#           2) plot_vlf(glob("objdir/Data/_VLF2_/y2012-m03-d29*"))
+#
+
+function plot_vlf(filenames)
+  if nargin != 1
+    filenames=glob("objdir/Data/_VLF2_/y2012-m03-d29*")
+  endif
+
+  ## qrgs [kHz]
+  qrgs   = [20.9           22.1                 24            23.4];
+  labels = {"g;20.9 HWU;", "r;22.1 Skeleton;", "m;24.0 NAA;", "b;23.4 DHO;"};
+
+  d=ps(filenames);
+  ##
+  ## df .. Frequenzmittelungsbreite
+  ##
+  df=0.05;  ## kHz
+
+  figure; hold on;
+  for i=1:length(qrgs)
+    idx=find(d.freq_Hz>1e3*(qrgs(i)-df) & d.freq_Hz<1e3*(qrgs(i)+df));
+    plot(d.time,mean(d.spec(:,idx)'), labels{i});
+  endfor
+  datetick('x', 'HH:MM');
+endfunction
