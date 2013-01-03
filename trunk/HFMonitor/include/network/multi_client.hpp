7de2773b2bdb69cc50e1447e981ef427368cf9f9
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2013-01-03 17:08:05 +0000

    added missing file

---
+++ trunk/HFMonitor/include/network/multi_client.hpp
@@ -0,0 +1,91 @@
+// -*- mode: C++; c-basic-offset: 2; indent-tabs-mode: nil  -*-
+// $Id$
+
+#ifndef _MULTI_CLIENT_HPP_cm121228_
+#define _MULTI_CLIENT_HPP_cm121228_
+
+#include <boost/regex.hpp>
+#include <string>
+
+#include "network.hpp"
+#include "network/client/client_base.hpp"
+#include "network/client/service_net.hpp"
+#include "processor.hpp"
+
+class multi_client : public client_base {
+public:
+  // map stream name -> processor sptr
+  typedef std::string stream_name;
+  typedef std::map<stream_name, processor::base::sptr> processor_id_map;
+
+  // regex (matching stream names) -> processor type
+  typedef std::string processor_type;
+  typedef std::vector<std::pair<boost::regex, processor_type> > processor_type_map;
+
+  // 
+
+  multi_client(const boost::property_tree::ptree& config)
+    : client_base(network::get_io_service(), config)
+    , config_(config) {}
+  virtual ~multi_client() {}
+
+  bool connect_to(std::string stream_names,
+		  std::string processor_type,
+		  std::string extra_argument="") {
+    if (client_base::connect_to(stream_names) == false)
+      return false;
+    processor_type_map_.push_back(std::make_pair(boost::regex(stream_names), processor_type));
+    return true;
+  }
+
+  virtual void process(data_buffer_type::const_iterator begin,
+                       data_buffer_type::const_iterator end) {
+    const stream_name str_name(get_directory().stream_name_of(get_header().stream_number()));
+    processor_id_map::iterator i(processor_id_map_.find(str_name));
+    if (i == processor_id_map_.end())
+      return; // TODO: complain
+
+    // make up service object
+    processor::service_base::sptr sp(service_net::make(get_header()));
+    i->second->process(sp, begin, end);
+  }
+
+protected:
+  // notifies derived classes of an update of directory
+  virtual void directory_update(const broadcaster_directory& old_directory,
+                                const broadcaster_directory& new_directory) {
+    // remove processors not present anymore
+    for (processor_id_map::iterator i(processor_id_map_.begin()); i!=processor_id_map_.end();) {
+      if (not new_directory.contains(i->first))
+        processor_id_map_.erase(i++);
+      else 
+        ++i;
+    }
+    // construct new processors, if needed
+    for (broadcaster_directory::const_iterator i(new_directory.begin());
+         i!=new_directory.end(); ++i) {
+      const stream_name str_name(i->first);
+      // if not found: make new
+      if (processor_id_map_.find(str_name) != processor_id_map_.end()) continue;
+      processor_id_map_[str_name] = processor::registry::make(find_type_of_stream(str_name), config_);
+    }
+  }  
+
+  // 
+  processor_type find_type_of_stream(std::string stream_name) const {
+    processor_type_map::const_iterator end(processor_type_map_.end());
+    for (processor_type_map::const_iterator i(processor_type_map_.begin()); i!=end; ++i)
+      if (regex_match(stream_name, i->first))
+        return i->second;
+    throw std::runtime_error("stream not found");
+  }
+
+private:
+  const boost::property_tree::ptree config_;   // holds copy of config 
+
+  processor_id_map   processor_id_map_;
+  processor_type_map processor_type_map_;
+} ;
+
+
+#endif // _MULTI_CLIENT_HPP_cm121228_
