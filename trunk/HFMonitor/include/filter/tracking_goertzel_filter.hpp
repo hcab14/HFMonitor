795a6876fb85ad90bf6b3b0fa90b948a584ff3b3
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2013-02-11 20:55:48 +0000

    intermediate

--- trunk/HFMonitor/include/filter/tracking_goertzel_filter.hpp
+++ trunk/HFMonitor/include/filter/tracking_goertzel_filter.hpp
@@ -13,9 +13,40 @@
 #include <boost/noncopyable.hpp>
 #include "filter/goertzel.hpp"
 
+namespace detail {
+  class value_and_error {
+  public:
+    value_and_error()
+      : valid_(false)
+      , value_(0.0)
+      , rms_value_(1e10) {}
+
+    value_and_error(double value,
+		    double rms_value)
+      : valid_(true)
+      , value_(value)
+      , rms_value_(rms_value) {}
+
+    bool valid() const { return valid_; }
+    double value() const { return value_; }
+    double rms_value() const { return rms_value_; }
+
+    friend std::ostream& operator<<(std::ostream& os, const value_and_error& ve) {
+      if (not ve.valid()) return os << "(invalid)";
+      return os << "( " << std::setprecision(15) << ve.value() << " +- " << ve.rms_value() << ")";
+    }
+
+  private:
+    bool   valid_;
+    double value_;
+    double rms_value_;
+  } ;
+} // namespace detail
+
 class tracking_goertzel_filter : private boost::noncopyable {
 public:
   typedef boost::shared_ptr<tracking_goertzel_filter> sptr;
+
   struct state {
     typedef enum {
       UNDEFINED  = 0,
@@ -27,22 +58,21 @@ public:
   } ;
 
   typedef std::complex<double> complex_t;
-  tracking_goertzel_filter(double fs,     // sampling frequency (Hz)
-			   double f0,     // center frequency (Hz)
-			   double df,     // initial resolution (Hz): +- df
-			   double min_df, // maximal frequency resolution (Hz)
-			   size_t max_history_size) // maximal size of history in seconds
 
-    : fs_(fs)
-    , period_(size_t(0.5+fs/df+0.5))
-    , min_period_(period_)
-    , max_period_(size_t(0.5+fs/min_df))
-    , left_((f0-df)/fs)
-    , center_(f0/fs)
-    , right_((f0+df)/fs)
-    , sample_counter_(0)
-    , last_state_(state::UNDEFINED)
-    , max_history_size_(size_t(0.5+fs*max_history_size)) {}
+  static sptr make(double fs,              // sampling frequency (Hz)
+		   double f0,              // center frequency (Hz)
+		   double df,              // initial resolution (Hz): +- df
+		   double min_df,          // maximal frequency resolution (Hz)
+		   size_t max_hist_size) { // maximal size of history in seconds
+    return sptr(new tracking_goertzel_filter(fs, f0, df, min_df, max_hist_size));
+  }
+
+  double         fs_Hz()         const { return fs_; }
+  state::state_t last_state()    const { return last_state_; }
+  bool           state_updated() const { return sample_counter_ == 0 && last_state() != state::UNDEFINED; }
+  size_t         period()        const { return period_; }
+  const detail::value_and_error& estimated_f_Hz() const { return estimated_f_; }
+  const detail::value_and_error& estimated_df()   const { return estimated_df_; }  
 
   void update(complex_t sample) {
     ++sample_counter_;
@@ -61,7 +91,7 @@ public:
 	phases_.push_back(std::make_pair(period_, std::arg(center_.x())));
 	compute_exact_frequency();
 	if (std::max(b/a, b/c) > 1.1 && period_ < max_period_) {
-	  period_ *=  2;
+	  period_ *= 2;
 	  left_  = goertzel_t(left_.kN()  + 1./period_);
 	  right_ = goertzel_t(right_.kN() - 1./period_);
 	}
@@ -104,23 +134,32 @@ public:
   }
 
 protected:
-  double compute_exact_frequency() {
-    if (phases_.empty()) return 0;
-    size_t history_size(0);
+  size_t history_size() const {
+    size_t hs(0);
     for (phase_vector_t::const_iterator i(phases_.begin()); i!= phases_.end(); ++i)
-      history_size += i->first;
+      hs += i->first;
+    return hs;
+  }
 
-    if (history_size > max_history_size_) 
+  void compute_exact_frequency() {
+    // reset the state
+    estimated_f_  = detail::value_and_error();
+    estimated_df_ = detail::value_and_error();
+    if (phases_.empty()) 
+      return;
+    
+    while (history_size() > max_history_size_)
       phases_.pop_front();
-
-    if (phases_.begin() == phases_.end()-1)
-      return fs_*center_.kN();
+    
+    if (phases_.size() == 1) {
+      estimated_f_ = detail::value_and_error(fs_*center_.kN(), 1e10);
+      return;
+    }
 
     double_t  sum_1(0),  sum_x(0),  sum_xx(0);
     double_t dsum_1(0), dsum_x(0), dsum_xx(0);
-
-    phase_vector_t::const_iterator end(phases_.end()-1);
     double_t last_df(0);
+    phase_vector_t::const_iterator end(phases_.end()-1);
     for (phase_vector_t::const_iterator i(phases_.begin()); i!=end; ++i) {
       phase_vector_t::const_iterator j(i+1);
       const double phase_diff(mod_2pi(j->second - i->second));
@@ -140,13 +179,14 @@ protected:
     }
     const double df(sum_x/sum_1);
     const double rms_df(sqrt(sum_xx/sum_1-df*df));
+    estimated_f_ = detail::value_and_error(fs_*center_.kN()+df, rms_df);
 
-    const double ddf(dsum_1 != 0.0 ? dsum_x/dsum_1 : 0.);
-    const double rms_ddf(dsum_1 != 0.0 ? sqrt(dsum_xx/dsum_1-ddf*ddf) : 0.);
-
-    std::cout << "\n## " << std::setprecision(15)<< fs_*center_.kN()+df << " " << rms_df << " "
-	      << ddf << " " << rms_ddf << std::endl;
-    return fs_*center_.kN()+df;
+    if (0.0 != dsum_1) {
+      const double ddf(dsum_x/dsum_1);
+      const double rms_ddf(sqrt(dsum_xx/dsum_1-ddf*ddf));
+      estimated_df_ = detail::value_and_error(ddf, rms_ddf);
+    }
+    std::cout << "\n## f,df: " << estimated_f_ << " " << estimated_df_ << std::endl;
   }
 
 private:
@@ -155,22 +195,40 @@ private:
     while (x >   M_PI) x -= 2*M_PI;
     return x;
   }
+  tracking_goertzel_filter(double fs,            // sampling frequency (Hz)
+			   double f0,            // center frequency (Hz)
+			   double df,            // initial resolution (Hz): +- df
+			   double min_df,        // maximal frequency resolution (Hz)
+			   size_t max_hist_size) // maximal size of history in seconds
+
+    : fs_(fs)
+    , period_(size_t(0.5+fs/df+0.5))
+    , min_period_(period_)
+    , max_period_(size_t(0.5+fs/min_df))
+    , left_((f0-df)/fs)
+    , center_(f0/fs)
+    , right_((f0+df)/fs)
+    , sample_counter_(0)
+    , last_state_(state::UNDEFINED)
+    , max_history_size_(size_t(0.5+fs*max_hist_size)) {}
 
   typedef goertzel<complex_t> goertzel_t;
   typedef std::deque<std::pair<size_t, double> > phase_vector_t;
 
-  double         fs_;
-  size_t         period_;
-  size_t         min_period_;
-  size_t         max_period_;
-  goertzel_t     left_;
-  goertzel_t     center_;
-  goertzel_t     right_;
-  size_t         sample_counter_;
-  state::state_t last_state_;
-  phase_vector_t phases_;
-  size_t         max_history_size_;
-  size_t         history_size_;
+  double          fs_;
+  size_t          period_;
+  size_t          min_period_;
+  size_t          max_period_;
+  goertzel_t      left_;
+  goertzel_t      center_;
+  goertzel_t      right_;
+  size_t          sample_counter_;
+  state::state_t  last_state_;
+  detail::value_and_error estimated_f_;
+  detail::value_and_error estimated_df_;
+  phase_vector_t  phases_;
+  size_t          max_history_size_;
+  size_t          history_size_;
 } ;
 
 #endif // _TRACKING_GOERTZEL_FILTER_HPP_cm20121104_
