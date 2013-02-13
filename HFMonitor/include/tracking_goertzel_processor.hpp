44a57a385550e1ab186330ba10abc2c1054db116
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2013-02-13 21:45:26 +0000

    intermediate

--- HFMonitor/include/tracking_goertzel_processor.hpp
+++ HFMonitor/include/tracking_goertzel_processor.hpp
@@ -3,9 +3,11 @@
 #ifndef _TRACKING_GOERTZEL_PROCESSOR_HPP_cm130211_
 #define _TRACKING_GOERTZEL_PROCESSOR_HPP_cm130211_
 
-#include <iostream>
-#include <vector>
 #include <complex>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
 #include <boost/property_tree/ptree.hpp>
 
 #include "filter/tracking_goertzel_filter.hpp"
@@ -15,6 +17,63 @@
 class tracking_goertzel_processor : public processor::base_iq {
 public:
   typedef boost::shared_ptr<tracking_goertzel_processor> sptr;
+
+  class result : public processor::result_base {
+  public:
+    typedef boost::shared_ptr<result> sptr;
+
+    virtual ~result() {}
+    static sptr make(std::string name,
+                     ptime t,
+                     double f0_Hz,
+                     std::string state,
+                     double period_sec,
+                     const detail::value_and_error& f_Hz,
+                     const detail::value_and_error& df) {
+      return sptr(new result(name, t, f0_Hz, state, period_sec, f_Hz, df));
+    }
+
+    double      f0_Hz()      const { return f0_Hz_; }
+    std::string state()      const { return state_; }
+    double      period_sec() const { return period_sec_; }
+    const detail::value_and_error& f_Hz() const { return f_Hz_; }
+    const detail::value_and_error& df()   const { return df_; }
+
+    virtual std::string to_string() const {
+      std::stringstream ss; 
+      ss << result_base::to_string() 
+         << " f0[Hz]="    << f0_Hz()
+         << " state="     << state()
+         << " period[s]=" << period_sec()
+         << " f[Hz]="     << f_Hz()
+         << " df="        << df();
+      return ss.str();
+    }
+
+  protected:
+  private:
+    result(std::string name,
+           ptime t,
+           double f0_Hz,
+           std::string state,
+           double period_sec,
+           const detail::value_and_error& f_Hz,
+           const detail::value_and_error& df)
+      : result_base(name, t)
+      , f0_Hz_(f0_Hz)
+      , state_(state)
+      , period_sec_(period_sec)
+      , f_Hz_(f_Hz)
+      , df_(df) {}
+
+    const double f0_Hz_;
+    const std::string state_;
+    const double period_sec_;
+    const detail::value_and_error f_Hz_;
+    const detail::value_and_error df_;
+  } ;
+
+  typedef boost::posix_time::time_duration time_duration;
 
   tracking_goertzel_processor(const boost::property_tree::ptree& config)
     : base_iq(config)
@@ -41,27 +100,27 @@ public:
                                                df_Hz_,
                                                min_df_Hz_,
                                                max_history_size_);
-    for (; i0!=i1; ++i0) {
-      filter_->update(*i0);
+    for (const_iterator i(i0); i!=i1; ++i) {
+      filter_->update(*i);
       if (filter_->state_updated()) {
-        const std::string state(tracking_goertzel_filter::state2str(filter_->last_state()));
-        const double period_sec(double(filter_->period())/double(sp->sample_rate_Hz()));
-        const detail::value_and_error f_Hz(filter_->estimated_f_Hz());
-        const detail::value_and_error df(filter_->estimated_df());
-        std::cout << "TGF updated: " << state
-                  << " period[s]= " << period_sec
-                  << " f[Hz]= " << f_Hz
-                  << " df[1]= " << df << std::endl;
+        const time_duration dt(0,0,0, std::distance(i0, i)*time_duration::ticks_per_second()/sp->sample_rate_Hz());
+        sp->put_result(result::make(sp->stream_name(),
+                                    sp->approx_ptime()+dt,
+                                    f0_Hz,
+                                    tracking_goertzel_filter::state2str(filter_->last_state()),
+                                    double(filter_->period())/double(sp->sample_rate_Hz()),
+                                    filter_->estimated_f_Hz(),
+                                    filter_->estimated_df()));
       }
     }
   }
 
 protected:
 private:
-  const double f0_Hz_;
-  const double df_Hz_; // initial df
-  const double min_df_Hz_;
-  const size_t max_history_size_;
+  const double f0_Hz_;             // initial middle frequency
+  const double df_Hz_;             // initial df: [f0+-df]
+  const double min_df_Hz_;         // maximal frequency resolution
+  const size_t max_history_size_;  // maximal phase history size in seconds
   tracking_goertzel_filter::sptr filter_;
 } ;
 
