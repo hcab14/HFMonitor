5d1728fc1706d46493e7cc2861423d13297e69c8
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2013-02-19 17:09:19 +0000

    added missing GNU license notices

--- trunk/HFMonitor/src/perseus_server_main.cpp
+++ trunk/HFMonitor/src/perseus_server_main.cpp
@@ -1,5 +1,21 @@
 // -*- mode: c++; c-basic-offset: 2; indent-tabs-mode: nil  -*-
 // $Id$
+//
+// Copyright 2010-2013 Christoph Mayer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 #include <iostream>
 
 #include <boost/date_time/posix_time/posix_time.hpp>
@@ -170,7 +186,7 @@ int main(int argc, char* argv[])
       } // here the destructor of wait_for_signal waits for a signal
 
 
-      rec->stop_async_input();      
+      rec->stop_async_input();
       buf->stop();
       bc->stop();
       network::get_io_service().stop();
