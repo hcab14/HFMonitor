4c5329496719ee2831109a350f205351af8b1ea6
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2013-02-27 21:53:22 +0000

    new version; TO BE TESTED in Ntz

--- HFMonitor/src/perseus_server_main.cpp
+++ HFMonitor/src/perseus_server_main.cpp
@@ -67,20 +67,22 @@ public:
     , strand_(broadcaster->get_strand())
     , receiver_(receiver)
     , do_run_(true)
-    , mutex_(new boost::mutex)
-    , cond_(new boost::condition) {}
+//     , mutex_(new boost::mutex)
+//     , cond_(new boost::condition) 
+  {}
 
   ~bridge() {}
 
   void operator()() {
     while (do_run_) {
       try {
-        boost::mutex::scoped_lock lock(*mutex_);
+//         boost::mutex::scoped_lock lock(*mutex_);
         data_= buffer_->get(); // this throws an exception if the buffer is stopped
-        strand_.post(strand_.wrap(boost::bind(&bridge::broadcast_data, this)));
+        strand_.dispatch(strand_.wrap(boost::bind(&bridge::broadcast_data, this)));
+//         strand_.post(strand_.wrap(boost::bind(&bridge::broadcast_data, this)));
         do_run_ = !strand_.get_io_service().stopped();
-        if (do_run_)
-          cond_->wait(lock);
+//         if (do_run_)
+//           cond_->wait(lock);
       } catch (...) {
         do_run_= false;
       }
@@ -89,7 +91,7 @@ public:
   }
 protected:
   void broadcast_data() {
-    boost::mutex::scoped_lock lock(*mutex_);
+//     boost::mutex::scoped_lock lock(*mutex_);
     // make up data packet
     const iq_info header_iq(receiver_->get_sample_rate(),
                             receiver_->get_center_frequency_hz(),
@@ -100,7 +102,7 @@ protected:
     std::copy(header_iq.begin(),    header_iq.end(),    std::back_inserter(d));
     std::copy(data_.second.begin(), data_.second.end(), std::back_inserter(d));
     broadcaster_->bc_data(data_.first, stream_name, "IQ__0000", d);
-    cond_->notify_one();
+//     cond_->notify_one();
   }
 
 private:
@@ -132,7 +134,7 @@ int main(int argc, char* argv[])
 
     if (num_perseus == 0)
       throw std::runtime_error("no perseus receiver found");    
-    
+
     int index(-1);
     for (size_t i(0); i<num_perseus; ++i) {
       const Perseus::product_id pid(Perseus::receiver_control::get_product_id_at(i, perseus_firmware));
@@ -150,34 +152,34 @@ int main(int argc, char* argv[])
         // wait until a signal is sent/service is stopped
         wait_for_signal w(network::get_io_service());
         w.add_signal(SIGINT).add_signal(SIGQUIT).add_signal(SIGTERM);        
-        
+
         const boost::property_tree::ptree& config_broadcaster(config.get_child("Broadcaster"));
         bc = broadcaster::make(config_broadcaster);
 
         bc->start();
-        
+
         // buffer between perseus and broadcaster
         buf = buffer<std::string>::make_buffer(1024*1000, boost::posix_time::seconds(1));
-        
+
         // set up receiver
         Perseus::callback::sptr cb(new test_cb(buf));
         rec = Perseus::receiver_control::make(index);
         rec->init(config_perseus);
-        
+
         const size_t thread_pool_size(config_broadcaster.get<size_t>("<xmlattr>.threadPoolSize"));
         for (std::size_t i(0); i<thread_pool_size; ++i) {
           boost::shared_ptr<boost::thread> thread
             (new boost::thread(boost::bind(&boost::asio::io_service::run, &network::get_io_service())));
           threads.push_back(thread);
         }
-        
+
         // bridge running in a thread
         {
           bridge r(buf, bc, rec);
           boost::shared_ptr<boost::thread> bridge_thread(new boost::thread(r));
           threads.push_back(bridge_thread);
         }
-        
+
         // start receiver callback
         rec->start_async_input(cb);
         
