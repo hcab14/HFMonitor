f22330238a6a58801e6d792ae5bb97bb18f7b9f8
vlfmonitor <vlfmonitor@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2014-04-25 12:01:13 +0000

    cosmetic update

--- tags/HFMonitor_v3.0/display_spec.sh
+++ tags/HFMonitor_v3.0/display_spec.sh
@@ -3,7 +3,6 @@
 
 # starts fltk_spec
 
-
 source functions.sh
 
 function find_port {
@@ -15,16 +14,16 @@ port=`find_port`
 
 case $1 in
     EFR)
-	./bin/fltk_spec -p $port -s DC_132000_EFR -o 50 -d 0.4 --fMin_kHz 127 --fMax_kHz 140 --sMin_dB -57 --sMax_dB -45
+	./bin/fltk_spec -p $port -s DC_132000_EFR  -o 50 -d 0.4 --fMin_kHz 127 --fMax_kHz 140 --sMin_dB -57 --sMax_dB -45
 	;;
     DCF77)
-	./bin/fltk_spec -p $port -s DC_077500_DCF -o 50 -d 0.25 --fMin_kHz  76 --fMax_kHz 79 --sMin_dB -57 --sMax_dB -45
+	./bin/fltk_spec -p $port -s DC_077500_DCF  -o 50 -d 0.25 --fMin_kHz 76 --fMax_kHz  79 --sMin_dB -57 --sMax_dB -45
 	;;
     MSF)
-	./bin/fltk_spec -p $port -s DC_060000_MSF -o 50 -d 0.25 --fMin_kHz  58 --fMax_kHz 62 --sMin_dB -57 --sMax_dB -45
+	./bin/fltk_spec -p $port -s DC_060000_MSF  -o 50 -d 0.25 --fMin_kHz 58 --fMax_kHz  62 --sMin_dB -57 --sMax_dB -45
 	;;
     NavyMSK)
-	./bin/fltk_spec -p $port -s DC_020000_MSKs -o 50 -d 1 --fMin_kHz 15 --fMax_kHz 25 --sMin_dB -75 --sMax_dB -45
+	./bin/fltk_spec -p $port -s DC_020000_MSKs -o 50 -d 1    --fMin_kHz 15 --fMax_kHz  25 --sMin_dB -75 --sMax_dB -45
 	;;
     *)
 	echo Unknown signal specification. Choose one from: EFR,DCF77,MSF,NavyMSK
