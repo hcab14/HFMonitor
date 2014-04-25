f5ff11afca73a134f9d70aa955e48bb46b200196
cmayer <cmayer@e777e3f1-5090-4870-b97d-402f25bdd3cb>
2014-04-25 11:59:57 +0000

    update display_spec

--- tags/HFMonitor_v3.0/display_spec.sh
+++ tags/HFMonitor_v3.0/display_spec.sh
@@ -15,13 +15,13 @@ port=`find_port`
 
 case $1 in
     EFR)
-	./bin/fltk_spec -p $port -s DC_132000_EFR -o 50 -d 0.4 --fMin_kHz 125 --fMax_kHz 140 --sMin_dB -57 --sMax_dB -45
+	./bin/fltk_spec -p $port -s DC_132000_EFR -o 50 -d 0.4 --fMin_kHz 127 --fMax_kHz 140 --sMin_dB -57 --sMax_dB -45
 	;;
     DCF77)
-	./bin/fltk_spec -p $port -s DC_077500_DCF -o 75 -d 0.5 --fMin_kHz  77 --fMax_kHz 78 --sMin_dB -57 --sMax_dB -45
+	./bin/fltk_spec -p $port -s DC_077500_DCF -o 50 -d 0.25 --fMin_kHz  76 --fMax_kHz 79 --sMin_dB -57 --sMax_dB -45
 	;;
     MSF)
-	./bin/fltk_spec -p $port -s DC_060000_MSF -o 75 -d 0.5 --fMin_kHz  58 --fMax_kHz 61 --sMin_dB -57 --sMax_dB -45
+	./bin/fltk_spec -p $port -s DC_060000_MSF -o 50 -d 0.25 --fMin_kHz  58 --fMax_kHz 62 --sMin_dB -57 --sMax_dB -45
 	;;
     NavyMSK)
 	./bin/fltk_spec -p $port -s DC_020000_MSKs -o 50 -d 1 --fMin_kHz 15 --fMax_kHz 25 --sMin_dB -75 --sMax_dB -45
